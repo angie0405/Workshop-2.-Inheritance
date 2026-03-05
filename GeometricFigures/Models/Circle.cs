5e2f96eb7c0469e52b36a873c368d202a1f86a00
Angie Manuela Marin <Angie Manuela Marin@ANG-2021>
2026-03-05 01:31:53 -0500

    Parent class and circle class

---
+++ GeometricFigures/Models/Circle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeometricFigures.Models
+{
+    internal class Circle : GeometricFigures
+    {
+        // Properties
+        private double radius;
+
+        // constructor
+        public Circle(double radius) :
+        base("Circle")
+        {
+            this.radius = radius;
+        }
+        //methods
+        public override double GetArea()
+        {
+            return Math.PI * radius * radius;
+        }
+
+        public override double GetPerimeter()
+        {
+            return 2 * Math.PI * radius;
+        }
+    }
+}
---
+++ GeometricFigures/Models/GeometricFigures.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeometricFigures.Models
+{
+    public abstract class GeometricFigures
+    {
+        public string Name { get; protected set; }
+
+        protected GeometricFigures(string name)
+        {
+            Name = name;
+        }
+
+        public abstract double GetArea();
+        public abstract double GetPerimeter();
+
+        public override string ToString()
+        {
+            return $"¨{Name} => Area.....:{GetArea():F5}     Perimeter:{GetPerimeter():F5}";
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
